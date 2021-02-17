879c9f6a710ccee5bf878352c28e6e5c3c21ec8b
Luke Dowell <lukedowell@gmail.com>
2021-02-17 01:26:03 -0600

    fix compilation errors

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use bevy::prelude::*;
 
-struct Position { x: i32, y: i32 }
+struct Position { x: f32, y: f32 }
 struct Renderable {
     glyph: String,
     fg: Color,
@@ -14,33 +14,46 @@ fn main() {
         .add_startup_system(setup.system())
         .add_system(position_sync.system())
         .add_system(input.system())
+        .add_resource(InputTimer(Timer::from_seconds(1.0, true)))
         .run();
 }
 
 fn setup(cmd: &mut Commands, asset_server: Res<AssetServer>) {
-    cmd.spawn(CameraUiBundle::default())
+    cmd.spawn(Camera2dBundle::default())
+        .spawn(CameraUiBundle::default())
         .spawn(TextBundle {
             text: Text {
-                sections: vec![
-                TextSection {
-                    value: "@",
-                    style: TextStyle
+                value: "@".to_string(),
+                font: asset_server.load("MajorMonoDisplay-Regular.ttf"),
+                style: TextStyle {
+                    color: Color::WHITE,
+                    font_size: 60.0,
+                    ..Default::default()
                 }
-                ],
-                ..Default::default()
             },
             ..Default::default()
         })
-        .with(Position { x: 40, y: 25 });
+        .with(Position { x: 40.0, y: 25.0 });
 
     for i in 0..10 {
-        cmd.with(Position { x: i * 7, y: 20 })
+        cmd.spawn(TextBundle {
+            text: Text {
+                value: "%".to_string(),
+                font: asset_server.load("MajorMonoDisplay-Regular.ttf"),
+                style: TextStyle {
+                    color: Color::BLUE,
+                    font_size: 60.0,
+                    ..Default::default()
+                }
+            },
+            ..Default::default()
+        }).with(Position { x: i as f32 * 7.0, y: 20.0 });
     }
 }
 
 struct InputTimer(Timer);
 fn input(time: Res<Time>,
-         timer: ResMut<InputTimer>,
+         mut timer: ResMut<InputTimer>,
          keyboard_input: Res<Input<KeyCode>>,
          mut query: Query<(&Player, &mut Position)>) {
 
@@ -48,19 +61,27 @@ fn input(time: Res<Time>,
         return;
     }
 
-    for (_player, mut position) in query.iter() {
+    for (_player, mut position) in query.iter_mut() {
         if keyboard_input.pressed(KeyCode::Left) {
-            position -= 1.0;
+            position.x -= 1.0;
         }
 
         if keyboard_input.pressed(KeyCode::Right) {
-            position += 1.0;
+            position.x += 1.0;
+        }
+
+        if keyboard_input.pressed(KeyCode::Up) {
+            position.y += 1.0;
+        }
+
+        if keyboard_input.pressed(KeyCode::Down) {
+            position.y -= 1.0;
         }
     }
 }
 
-fn position_sync(query: Query<(&Position, &mut Transform)>) {
-    for (position, transform) in query.iter() {
+fn position_sync(mut query: Query<(&Position, &mut Transform)>) {
+    for (position, mut transform) in query.iter_mut() {
         transform.translation = Vec3::new(position.x * 20 as f32, position.y * 20 as f32, 0.0)
     }
 }
